58bd2125a4f6ec49c6949dcf2a5553d7d7026425
Mark Young <Markyjay@gmail.com>
2023-08-11 12:43:13 +0000

    add function for deal and hit button

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ function getRandomCard() {
     const randomSuit = suits[Math.floor(Math.random() * suits.length)];
     const randomValue = values[Math.floor(Math.random() * values.length)];
     return randomValue + randomSuit;
-  }
-  
+}
+
+function deal() {
+    card1 = getRandomCard();
+    card2 = getRandomCard();
+    card3 = 'blank';
+    document.getElementById('card1').textContent = card1;
+    document.getElementById('card2').textContent = card2;
+    document.getElementById('card3').textContent = card3;
+}
+
+function hit() {
+    card3 = getRandomCard();
+    document.getElementById('card3').textContent = card3;
+    checkResult();
+}
