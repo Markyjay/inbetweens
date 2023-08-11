e6a26feb316e23d99d05846861d1b657c93b7be8
Mark Young <Markyjay@gmail.com>
2023-08-11 12:35:40 +0000

    add a get random card function to javascript file

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const suits = ['♠', '♥', '♦', '♣'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
 let credits = 50;
 let bet = 5;
 let card1, card2, card3;
 
-const suits = ['♠', '♥', '♦', '♣'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+function getRandomCard() {
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+    return randomValue + randomSuit;
+  }
+  
