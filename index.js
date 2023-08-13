cf3ad0c5dbbf76bb4bc799c237e9d0b81cdb78a9
Mark Young <Markyjay@gmail.com>
2023-08-13 15:28:19 +0000

    alter deal and hit function URL

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
 const suits = ['♠', '♥', '♦', '♣'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', 't', 'j', 'q', 'k', 'a'];
 
 const cardImages = {
-    '2c' : '2c.JPG',
-    '3c' : '3c.JPG',
-    '4c' : '4c.JPG',
-    '5c' : '5c.JPG',
-    '6c' : '6c.JPG',
-    '7c' : '7c.JPG',
-    '8c' : '8c.JPG',
-    '9c' : '9c.JPG',
-    'tc' : 'tc.JPG',
-    'jc' : 'jc.JPG',
-    'qc' : 'qc.JPG',
-    'kc' : 'kc.JPG',
-    'ac' : 'ac.JPG',
-    '2d' : '2d.JPG',
-    '3d' : '3d.JPG',
-    '4d' : '4d.JPG',
-    '5d' : '5d.JPG',
-    '6d' : '6d.JPG',
-    '7d' : '7d.JPG',
-    '8d' : '8d.JPG',
-    '9d' : '9d.JPG',
-    'td' : 'td.JPG',
-    'jd' : 'jd.JPG',
-    'qd' : 'qd.JPG',
-    'kd' : 'kd.JPG',
-    'ad' : 'ad.JPG',
-    '2h' : '2h.JPG',
-    '3h' : '3h.JPG',
-    '4h' : '4h.JPG',
-    '5h' : '5h.JPG',
-    '6h' : '6h.JPG',
-    '7h' : '7h.JPG',
-    '8h' : '8h.JPG',
-    '9h' : '9h.JPG',
-    'th' : 'th.JPG',
-    'jh' : 'jh.JPG',
-    'qh' : 'qh.JPG',
-    'kh' : 'kh.JPG',
-    'ah' : 'ah.JPG',
-    '2s' : '2s.JPG',
-    '3s' : '3s.JPG',
-    '4s' : '4s.JPG',
-    '5s' : '5s.JPG',
-    '6s' : '6s.JPG',
-    '7s' : '7s.JPG',
-    '8s' : '8s.JPG',
-    '9s' : '9s.JPG',
-    'ts' : 'ts.JPG',
-    'js' : 'js.JPG',
-    'qs' : 'qs.JPG',
-    'ks' : 'ks.JPG',
-    'as' : 'as.JPG',
+    '2♣' : '2c.JPG',
+    '3♣' : '3c.JPG',
+    '4♣' : '4c.JPG',
+    '5♣' : '5c.JPG',
+    '6♣' : '6c.JPG',
+    '7♣' : '7c.JPG',
+    '8♣' : '8c.JPG',
+    '9♣' : '9c.JPG',
+    't♣' : 'tc.JPG',
+    'j♣' : 'jc.JPG',
+    'q♣' : 'qc.JPG',
+    'k♣' : 'kc.JPG',
+    'a♣' : 'ac.JPG',
+    '2♦' : '2d.JPG',
+    '3♦' : '3d.JPG',
+    '4♦' : '4d.JPG',
+    '5♦' : '5d.JPG',
+    '6♦' : '6d.JPG',
+    '7♦' : '7d.JPG',
+    '8♦' : '8d.JPG',
+    '9♦' : '9d.JPG',
+    't♦' : 'td.JPG',
+    'j♦' : 'jd.JPG',
+    'q♦' : 'qd.JPG',
+    'k♦' : 'kd.JPG',
+    'a♦' : 'ad.JPG',
+    '2♥' : '2h.JPG',
+    '3♥' : '3h.JPG',
+    '4♥' : '4h.JPG',
+    '5♥' : '5h.JPG',
+    '6♥' : '6h.JPG',
+    '7♥' : '7h.JPG',
+    '8♥' : '8h.JPG',
+    '9♥' : '9h.JPG',
+    't♥' : 'th.JPG',
+    'j♥' : 'jh.JPG',
+    'q♥' : 'qh.JPG',
+    'k♥' : 'kh.JPG',
+    'a♥' : 'ah.JPG',
+    '2♠' : '2s.JPG',
+    '3♠' : '3s.JPG',
+    '4♠' : '4s.JPG',
+    '5♠' : '5s.JPG',
+    '6♠' : '6s.JPG',
+    '7♠' : '7s.JPG',
+    '8♠' : '8s.JPG',
+    '9♠' : '9s.JPG',
+    't♠' : 'ts.JPG',
+    'j♠' : 'js.JPG',
+    'q♠' : 'qs.JPG',
+    'k♠' : 'ks.JPG',
+    'a♠' : 'as.JPG',
 }
 
 let credits = 50;
@@ -88,15 +88,15 @@ function deal() {
     alert("Not enough credits to deal. Please change your bet or add more credits.");
     }
 
-    document.getElementById('card1').style.backgroundImage = `url(images/${card1}.JPG)`;
-    document.getElementById('card2').style.backgroundImage = `url(images/${card2}.JPG)`;
+    document.getElementById('card1').style.backgroundImage = 'url(images/${cardImages[card1]})';
+    document.getElementById('card2').style.backgroundImage = 'url(images/${cardImages[card2]})';
     document.getElementById('card3').style.backgroundImage = 'none'; // Clear card3 image
 }
 
 function hit() {
     card3 = getRandomCard();
     document.getElementById('card3').textContent = card3;
-    document.getElementById('card3').style.backgroundImage = `url(images/${card3}.JPG)`;
+    document.getElementById('card3').style.backgroundImage = 'url(images/${cardImages[card3]})';
     checkResult();
 }
 
