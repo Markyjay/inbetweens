a8b408415bcc7c00caf89ecc64ada2eb86d63c2b
Mark Young <Markyjay@gmail.com>
2023-09-01 14:56:14 +0000

    insert image to main heading

--- assets/js/instructions.js
+++ assets/js/instructions.js
@@ -69,43 +69,55 @@ const cardImages = {
   "12♠": "qs.jpg",
   "13♠": "ks.jpg",
   "1♠": "as.jpg",
-  "back": "back.jpg",
+  back: "back.jpg",
 };
 
 function getRandomCard() {
-    // Filter out unwanted cards
-    const filteredValues = values.filter(
-      (value) =>
-        !(value === "1" && (suits.includes("♥") || suits.includes("♦"))) &&
-        !(value === "14" && (suits.includes("♣") || suits.includes("♠")))
-    );
-  
-    // Select a random suit and value from the filtered arrays
-    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
-    const randomValue =
-      filteredValues[Math.floor(Math.random() * filteredValues.length)];
-  
-    return randomValue + randomSuit;
-  }
-  
-    let card1 = getRandomCard();
-    let card2 = getRandomCard();
-    let card3 = getRandomCard();
+  // Filter out unwanted cards
+  const filteredValues = values.filter(
+    (value) =>
+      !(value === "1" && (suits.includes("♥") || suits.includes("♦"))) &&
+      !(value === "14" && (suits.includes("♣") || suits.includes("♠")))
+  );
 
-    const value1 = values.indexOf(card1.substring(0, card1.length - 1));
-    const value2 = values.indexOf(card2.substring(0, card2.length - 1));
-    const value3 = values.indexOf(card3.substring(0, card3.length - 1));
+  // Select a random suit and value from the filtered arrays
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue =
+    filteredValues[Math.floor(Math.random() * filteredValues.length)];
 
-function calculateProbability(value1, value2, value3) {
-    const sortedValues = [value1, value2].sort((a, b) => a - b);
-
-    if (sortedValues[0] < value3 && value3 < sortedValues[1]) {
-        return 1; // In-between condition satisfied
-    } else {
-        return 0; // In-between condition not satisfied
-    }
+  return randomValue + randomSuit;
 }
 
+let card1 = getRandomCard();
+let card2 = getRandomCard();
+let card3 = getRandomCard();
+
+const value1 = values.indexOf(card1.substring(0, card1.length - 1));
+const value2 = values.indexOf(card2.substring(0, card2.length - 1));
+const value3 = values.indexOf(card3.substring(0, card3.length - 1));
+
+function calculateProbability(value1, value2, value3) {
+  const sortedValues = [value1, value2].sort((a, b) => a - b);
+  document.getElementById(
+    "card1"
+  ).style.backgroundImage = `url(assets/images/${cardImages[card1]})`;
+  document.getElementById(
+    "card2"
+  ).style.backgroundImage = `url(assets/images/${cardImages[card2]})`;
+
+  if (sortedValues[0] < value3 && value3 < sortedValues[1]) {
+    return 1; // In-between condition satisfied
+  } else {
+    return 0; // In-between condition not satisfied
+  }
+}
 const probability = calculateProbability(value1, value2, value3);
 
-console.log(`Probability that the third card's value is in between the first two cards' values: ${probability}`);
+document.addEventListener("DOMContentLoaded", function () {
+  const probability = document.getElementById("calculate");
+  probability.addEventListener("click", calculate);
+});
+
+console.log(
+  `Probability that the third card's value is in between the first two cards' values: ${probability}`
+);
